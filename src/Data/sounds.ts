5ba86e52a30ace39a49c4b1b28198735121b75a9
Daniel Machado <daniel7oficial@gmail.com>
2025-01-09 13:48:33 -0300

    Ajustando sons para o teste

--- src/Data/sounds.ts
+++ src/Data/sounds.ts
@@ -1,7 +1,7 @@
 export const sounds = [
-  { audio: "../assets/sounds/bird.mp3", correctAnswer: "Ave", options: ["Gato", "Ave", "Elefante", "Gorila"] },
-  { audio: "../assets/sounds/cat.mp3", correctAnswer: "Gato", options: ["Gato", "Cachorro", "Leão", "Cavalo"] },
-  { audio: "../assets/sounds/cock.mp3", correctAnswer: "Galinha", options: ["Gato", "Cachorro", "Galinha", "Cavalo"] },
-  { audio: "../assets/sounds/dog.mp3", correctAnswer: "Cachorro", options: ["Gato", "Cachorro", "Leão", "Cavalo"] },
-  { audio: "../assets/sounds/horse.mp3", correctAnswer: "Cavalo", options: ["Gato", "Cachorro", "Leão", "Cavalo"] },
+  { audio: "/sounds/bird.mp3", correctAnswer: "Ave", options: ["Gato", "Ave", "Elefante", "Gorila"] },
+  { audio: "/sounds/cat.mp3", correctAnswer: "Gato", options: ["Gato", "Cachorro", "Leão", "Cavalo"] },
+  { audio: "/sounds/cock.mp3", correctAnswer: "Galinha", options: ["Gato", "Cachorro", "Galinha", "Cavalo"] },
+  { audio: "/sounds/dog.mp3", correctAnswer: "Cachorro", options: ["Gato", "Cachorro", "Leão", "Cavalo"] },
+  { audio: "/sounds/horse.mp3", correctAnswer: "Cavalo", options: ["Gato", "Cachorro", "Leão", "Cavalo"] },
 ];
